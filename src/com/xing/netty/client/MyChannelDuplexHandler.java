e254046fe3bdc789df5ed59aa4c5b3823c923561
mfiianon <finnxing0516@gmail.com>
2015-01-06 14:47:06 +0800

    delete unused MyChannelDuplexHandler

--- src/com/xing/netty/client/MyChannelDuplexHandler.java
+++
@@ -1,9 +0,0 @@
-package com.xing.netty.client;
-
-import io.netty.channel.ChannelDuplexHandler;
-import io.netty.util.AttributeKey;
-
-public class MyChannelDuplexHandler extends ChannelDuplexHandler{
-
-	AttributeKey<Integer> key = new AttributeKey<>("");
-}
